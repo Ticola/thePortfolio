72cb85781cb10f654a1fb9d9d8c3637e09e2094f
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2019-04-14 16:49:12 -0500

    ER

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-/* ===================================================================
- * Glint - Main JS
- *
- * ------------------------------------------------------------------- */
-
 (function ($) {
 
     "use strict";
 
     var cfg = {
         scrollDuration: 800, // smoothscroll duration
-        mailChimpURL: 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+        mailChimpURL: 'https://gmail.us20.list-manage.com/subscribe/post?u=e8313bab510f1db6f89ad6cf9&amp;id=c368cfb4e8'   // mailchimp url
     },
 
         $WIN = $(window);
